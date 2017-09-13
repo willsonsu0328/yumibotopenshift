578f26aa80a0e6fcafda8921941c6dfac0cdd898
Wilson <wilson@linkwish.com>
2017-09-13 12:32:14 +0800

    test follow

--- app.py
+++ app.py
@@ -59,8 +59,8 @@ def callback():
     return 'OK'
 
 @handler.add(FollowEvent)
-    def follow(event):
-        p("follow event.type: "+event.type)
+def follow(event):
+    p("follow event.type: "+event.type)
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
