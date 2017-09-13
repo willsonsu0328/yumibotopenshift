3e53d3d5366ed12eba37dd2859434965aa16324c
Wilson <wilson@linkwish.com>
2017-09-13 12:26:17 +0800

    add follow, and unfollow event

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, FollowEvent, UnfollowEvent
 )
 from linebot.exceptions import LineBotApiError
 
